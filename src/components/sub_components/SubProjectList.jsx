76286d1f9be0c2504e87d72474824c5c8ac04c36
Enoch <enochstrok@outlook.com>
2021-04-02 02:26:22 -0700

    commented out + button inside sub-projects list

--- src/components/sub_components/SubProjectList.jsx
+++ src/components/sub_components/SubProjectList.jsx
@@ -120,7 +120,8 @@ const SubProjectList = props => {
                                                                     <li className="list-group-item">A second item</li>
                                                                     <li className="list-group-item">A third item</li>
                                                                     <li className="list-group-item">
-                                                                        <button type="button" className="btn btn-secondary" onClick={addNewProjectField}>+</button>
+                                                                        {/* <button type="button" className="btn btn-secondary" onClick={addNewProjectField}>+</button> */}
+                                                                        <button type="button" className="btn btn-secondary" >+</button>
                                                                     </li>
                                                                 </ul>
                                                             </div>
